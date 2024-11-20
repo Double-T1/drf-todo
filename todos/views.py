f27c8e1e9c461acade5244f3cac0beb8231eaf3f
will <newsforcwc@gmail.com>
2024-11-20 14:41:55 +0800

    add swagger doc

--- todos/views.py
+++ todos/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins, permissions
 from django.contrib.auth.models import User
 from .serializers import UserSerializer, TodoSerializer
 from .models import Todo
-from .permissions import IsOwner
+from .permissions import IsOwner, IsUser
 
 
 class UserViewSet(
@@ -14,6 +14,24 @@ class UserViewSet(
     ):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [permissions.IsAdminUser]
+
+        elif self.action == "create":
+            permission_classes = [permissions.AllowAny]
+
+        elif self.action == "retrieve" or (self.action == "destroy"):
+            permission_classes = [
+                permissions.IsAuthenticated,
+                IsUser | permissions.IsAdminUser
+            ]
+
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
 
 
 class TodoViewSet(viewsets.ModelViewSet):
